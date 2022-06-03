a9614a0b47a28976954e68887c59219d55de9e2e
ytrewq271828 <ytrewq271828@naver.com>
2022-06-03 07:33:41 +0000

    220603 - 1.0s/epoch

--- loss.py
+++ loss.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-
+'''
 def get_rec(norm_constant, weight, adj_matrix, generated_prob):
         adj_matrix=adj_matrix.to('cuda')
         generated_prob=generated_prob.to('cuda')
@@ -63,88 +63,105 @@ def loss(generated_prob, adj_matrix, mu, sigma, Z, epsilon, latent_representatio
             
     return rec_cost, log_prior_kernel, log_posterior_kernel
     '''
-    #VAE loss : https://github.com/kampta/pytorch-distributions/blob/master/gaussian_vae.py
-    def loss(self, input, reconstructed_input, qz):
-        K=2000
-        J=150
-        eps=1e-10
-        train_xs_shape=784
-        N=self.adj_matrix.size(0)
-        adj_train=self.adj_matrix
-        
-        #Drawing K samples from q(psi|X,A) - final shape : (N, K, J+1, 16=output_dim_mu=output_dim_sigma)
-        #J+1 copy of samples of shape (N, K, 16)
-        #z_logv : output of GIN network
-        #sigma_iw1 : sigma of qphi - since z_logv=log(sigma^2) 
-        #psi_iw : output of GIN network - mu of qphi
-        #Not explicitly evaluating qphi - implicit approach 
-        z_logv=self.sigma #(N, output_dim_sigma[-1])
-        z_logv_iw=torch.tile(torch.unsqueeze(z_logv, 1), [1,K,1]) #(N, K, output_dim_mu[-1])
-        sigma_iw1=torch.exp(z_logv_iw.type(torch.FloatTensor)/2) 
-        sigma_iw2=torch.tile(torch.unsqueeze(sigma_iw1, axis=2), [1, 1, J+1, 1]) #(N, K, J+1, output_dim_mu[-1])
-        psi_iw=self.mu #(N, K, output_dim_mu[-1]==output_dim_sigma[-1]) - original paper shape
-        psi_iw_vec=torch.mean(psi_iw, axis=1)
-        param=torch.normal(mean=0, std=1)
-        #sampling Z from q(Z|psi) - z_sample_iw is equal to the Z in the paper
-        z_sample_iw=self.mu+param*sigma_iw1 #(N, K, output_dim_mu[-1])
-        z_sample_iw2=torch.tile(torch.unsqueeze(z_sample_iw, axis=2), [1, 1, J+1, 1]) #(N, K, J+1, output_dim_mu[-1])
+#VAE loss : https://github.com/kampta/pytorch-distributions/blob/master/gaussian_vae.py
+def loss(generated_prob, adj_matrix, mu, sigma, Z, epsilon, latent_representation, K, J, WU, norm, weight, dropout, device='cuda'):   
+    eps=1e-10
+    train_xs_shape=1433 #number of features of Cora dataset
+    N=adj_matrix.size(0)
+    z_dim=mu.shape[2]
+    adj_train=adj_matrix
+    
+    #Drawing K samples from q(psi|X,A) - final shape : (N, K, J+1, 16=output_dim_mu=output_dim_sigma)
+    #J+1 copy of samples of shape (N, K, 16)
+    #z_logv : output of GIN network
+    #sigma_iw1 : sigma of qphi - since z_logv=log(sigma^2) 
+    #psi_iw : output of GIN network - mu of qphi
+    #Not explicitly evaluating qphi - implicit approach 
+    
+    #z_logv=sigma #(N, output_dim_sigma[-1])
+    #z_logv_iw=torch.tile(torch.unsqueeze(z_logv, 1), [1,K,1]) #(N, K, output_dim_mu[-1])
+    
+    sigma_iw1=sigma[:K, :, :] #(K, N, z_dim) 
+    z_logv_iw=torch.log(sigma_iw1+eps)*2
+    sigma_iw2=torch.tile(torch.unsqueeze(sigma_iw1, axis=0), [J+1, 1, 1, 1]) #(J+1, K, N, z_dim)
+    
+    psi_iw=mu[:K, :, :] #(K, N, z_dim)
+    psi_iw_vec=torch.mean(psi_iw, axis=0) #(N, z_dim)
+    param=torch.normal(mean=0.0, std=1.0, size=(K, N, z_dim)).to('cuda')
+    
+    #sampling Z from q(Z|psi) - z_sample_iw is equal to the Z in the paper
+    z_sample_iw=psi_iw+param*sigma_iw1 #(K, N, z_dim)
+    z_sample_iw2=torch.tile(torch.unsqueeze(z_sample_iw, axis=0), [J+1, 1, 1, 1]) #(J+1, K, N, z_dim)
 
-        #reuse=True => sample from same distribution => another sample of mu of qphi
-        psi_iw_star=self.encoder.GINmu.forward(self.adj_matrix, self.feat_matrix, 0, self.encoder.GINu.forward(self.adj_matrix, self.feat_matrix, J, 0)) #(N, J, output_dim_mu[-1])  
-        psi_iw_star1=torch.tile(torch.unsqueeze(psi_iw_star,axis=1), [1,K,1,1]) #(N, K, J, output_dim_mu[-1])
-        psi_iw_star2=torch.cat([psi_iw_star1, torch.unsqueeze(psi_iw, axis=2)],2) #(N, K, J+1, output_dim_mu[-1])
-        
-        #Bayesian kernel of normal distribution
-        #https://namyoungkim.github.io/statistics/2017/09/18/probability/
-        ker=torch.exp(-0.5 * torch.sum(torch.square(z_sample_iw2-psi_iw_star2)/torch.square(sigma_iw2+eps),3)) #(N, K, J+1)
-        
-        
-        log_H_iw_vec=torch.log(torch.mean(ker, axis=2)+eps)-0.5*torch.sum(z_logv_iw, 2) #(N,K)
-        log_H_iw=torch.mean(log_H_iw_vec, axis=0) #(K)
-        
-        log_prior_iw_vec=-0.5*torch.sum(torch.square(z_sample_iw),2) #(N,K)
-        log_prior_iw=torch.mean(log_prior_iw_vec, axis=0)  #(K)
-        
-        x=torch.randn(self.adj_matrix.size(0), train_xs_shape)
-        x_iw=torch.tile(torch.unsqueeze(x, axis=1),[1,K,1]) #(N, K, 784)
-        
-        for i in range(K):
-            input_=torch.squeeze(z_sample_iw[:,i,:])
-            logits_x=torch.nn.Sigmoid(self.decoder.runDecoder(input_))
-            if i==0:
-                outputs=torch.unsqueeze(logits_x, axis=2)
-            else:
-                outputs=torch.cat([outputs, torch.unsqueeze(logits_x, axis=2)], axis=2)
-        
-        logits_x_iw=outputs #(N, N, K) 
-        reconstruct_iw=logits_x_iw
-        
-        adjacency_orig_dense=adj_train+torch.eye(adj_train.shape[0])
-        adj_orig_tile=torch.unsqueeze(adjacency_orig_dense, -1)
-        adj_orig_tile=torch.tile(adj_orig_tile, mutiplies=[1,1,K]) #(N,N,K)
-        
-        pos_weight = float(self.adj_matrix.shape[0] * self.adj_matrix.shape[0] - self.adj_matrix.sum()) / self.adj_matrix.sum()
-        norm = self.adj_matrix.shape[0] * self.adj_matrix.shape[0] / float((self.adj_matrix.shape[0] * self.adj_matrix.shape[0] - self.adj_matrix.sum()) * 2)
-        
-        #weighted_cross_entropy_with_logits
-        #logit : reconstruct_iw
-        
-        log_lik_iw = -1 * norm * torch.mean(
-            (1-adj_orig_tile)*reconstruct_iw + 
-            (1+(pos_weight-1)*adj_orig_tile)*
-            torch.log(1+torch.exp(-1*abs(reconstruct_iw))+
-            max(-1*reconstruct_iw,0)),axis=[0,1]) #K
-        
-        loss_iw0=-torch.log(torch.sum(torch.exp(log_lik_iw+(log_prior_iw-log_H_iw)*0/N))) + torch.log(K.type(torch.Float32))
-        loss_iw=loss_iw0
-        #BCE=nn.Functional.binary_cross_entropy(reconstructed_input, input.view(-1, input.size(0)), reduction='mean')
-        #pz=torch.normal.Normal(torch.zeros_like(qz.loc), torch.ones_like(qz.scale))
-        #KLD=torch.distributions.kl_divergence(qz, pz).mean()
-        
-        #return BCE + KLD
-        BCE=nn.Functional.binary_cross_entropy(reconstructed_input, input.view(-1, input.size(0)), reduction='sum')
-        pz=torch.normal.Normal(torch.zeros_like(qz.loc), torch.ones_like(qz.scale))
-        KLD=torch.distributions.kl_divergence(qz, pz).sum()
-        
-        return BCE + KLD
-        '''
+    #reuse=True => sample from same distribution => another sample of mu of qphi
+    psi_iw_star=mu[K:, :, :] #(J, N, z_dim)  
+    psi_iw_star1=torch.tile(torch.unsqueeze(psi_iw_star,axis=1), [1,K,1,1]) #(J, K, N, z_dim)
+    psi_iw_star2=torch.cat([psi_iw_star1, torch.unsqueeze(psi_iw, axis=0)],0) #(J+1, K, N, z_dim)
+    
+    #Bayesian kernel of normal distribution
+    #https://namyoungkim.github.io/statistics/2017/09/18/probability/
+    ker=torch.exp(-0.5 * torch.sum(torch.square(z_sample_iw2-psi_iw_star2)/(torch.square(sigma_iw2)+eps),3)) #(J+1, K, N)
+    
+    
+    log_H_iw_vec=torch.log(torch.mean(ker, axis=0)+eps)-0.5*torch.sum(z_logv_iw, 2) #(K, N)
+    log_H_iw=torch.mean(log_H_iw_vec, axis=1) #(K)
+    
+    log_prior_iw_vec=-0.5*torch.sum(torch.square(z_sample_iw),2) #(K, N)
+    log_prior_iw=torch.mean(log_prior_iw_vec, axis=1)  #(K)
+    
+    x=torch.randn(adj_matrix.size(0), train_xs_shape)
+    x_iw=torch.tile(torch.unsqueeze(x, axis=0),[K,1,1]) #(K, N, 1433)
+    
+    for i in range(K):
+        input_=torch.squeeze(z_sample_iw[i, :,:])
+        rk=torch.cuda.FloatTensor(torch.Size([z_dim]))
+        torch.nn.init.uniform_(rk, a=-6., b=0.)
+        rk=torch.sigmoid(rk)
+        dropoutlayer=torch.nn.Dropout(p=dropout)
+        input_=dropoutlayer(input_)
+        x=torch.transpose(input_, 0, 1)
+        x=torch.matmul(torch.diag(rk), x)
+        x=torch.matmul(input_, x)
+        x=torch.clamp(x, min=float('-inf'), max=10)
+        logits_x=1-torch.exp(-torch.exp(x))
+        #logits_x=torch.nn.Sigmoid(self.decoder.runDecoder(input_))
+        if i==0:
+            outputs=torch.unsqueeze(logits_x, axis=0)
+        else:
+            outputs=torch.cat([outputs, torch.unsqueeze(logits_x, axis=0)], axis=0)
+    
+    logits_x_iw=outputs #(K, N, N) 
+    reconstruct_iw=logits_x_iw.to('cuda')
+    
+    adjacency_orig_dense=adj_train+torch.eye(adj_train.shape[0])
+    adj_orig_tile=torch.unsqueeze(adjacency_orig_dense, 0)
+    adj_orig_tile=torch.tile(adj_orig_tile, (K, 1, 1)).to('cuda') #(K, N, N)
+    
+    #pos_weight = float(self.adj_matrix.shape[0] * self.adj_matrix.shape[0] - self.adj_matrix.sum()) / self.adj_matrix.sum()
+    #norm = self.adj_matrix.shape[0] * self.adj_matrix.shape[0] / float((self.adj_matrix.shape[0] * self.adj_matrix.shape[0] - self.adj_matrix.sum()) * 2)
+    
+    #weighted_cross_entropy_with_logits
+    #logit : reconstruct_iw
+    '''
+    log_lik_iw = -1 * norm * torch.mean(
+        (1-adj_orig_tile)*reconstruct_iw + 
+        (1+(weight-1)*adj_orig_tile)*
+        torch.log(1+torch.exp(-1*abs(reconstruct_iw))+
+        max(-1*reconstruct_iw,0)),axis=[1,2]) #K
+    '''
+    log_lik_iw = norm * torch.mean(
+      adj_orig_tile * torch.log(reconstruct_iw + eps) *
+      weight + (1-adj_orig_tile) * torch.log(1 - reconstruct_iw + eps), axis=[1,2])
+
+    loss_iw0 = -torch.logsumexp(log_lik_iw+(log_prior_iw-log_H_iw)*WU/N+eps, dim=0, keepdim=False) + torch.log(torch.cuda.FloatTensor([K]))
+    loss_iw=loss_iw0
+    #BCE=nn.Functional.binary_cross_entropy(reconstructed_input, input.view(-1, input.size(0)), reduction='mean')
+    #pz=torch.normal.Normal(torch.zeros_like(qz.loc), torch.ones_like(qz.scale))
+    #KLD=torch.distributions.kl_divergence(qz, pz).mean()
+    
+    #return BCE + KLD
+    #BCE=torch.nn.Functional.binary_cross_entropy(reconstructed_input, input.view(-1, input.size(0)), reduction='sum')
+    #pz=torch.normal.Normal(torch.zeros_like(qz.loc), torch.ones_like(qz.scale))
+    #KLD=torch.distributions.kl_divergence(qz, pz).sum()
+    
+    return loss_iw
--- sigvaemodel.py
+++ sigvaemodel.py
@@ -25,11 +25,10 @@ def graph_generator(inputGraph, X):
 
 #Shape of input X : [1, N, M]
 #output_dims=[output dim of 1st layer, output dim of 2nd layer, ..., D]
-#GINConv : Readout not implemented - we should apply readout to the result
 #Shape of final result : [sample_size, N, D]
-class GIN(nn.Module):
+class GINWithNoise(nn.Module):
     def __init__(self, L, input_dim, output_dims, noise_dim, sample_size, activation, dropout=0):
-        super(GIN, self).__init__()
+        super(GINWithNoise, self).__init__()
         self.numLayer=L
         self.input_dim=input_dim
         self.output_dims=output_dims
@@ -42,16 +41,15 @@ class GIN(nn.Module):
         if len(self.output_dims)!=L:
             raise Exception("number of layer not matched to output dimensions")
         
-        self.GINLayers.append(dgl.nn.GINConv(apply_func=nn.Linear(self.input_dim+self.noise_dim+32,self.output_dims[0]), aggregator_type="sum", init_eps=0, learn_eps=True, activation=self.activation).to('cuda'))
+        self.GINLayers.append(dgl.nn.GINConv(apply_func=nn.Linear(self.input_dim+self.noise_dim,self.output_dims[0]), aggregator_type="sum", init_eps=0, learn_eps=True, activation=self.activation).to('cuda'))
         for i in range(L-1):
             self.GINLayers.append(dgl.nn.GINConv(apply_func=nn.Linear(self.output_dims[i], self.output_dims[i+1]),aggregator_type="sum", init_eps=0, learn_eps=True, activation=self.activation).to('cuda'))
     
     #return shape : (sample_size, N,D)
-    def forward(self, A, X, input_graph, h):
+    def forward(self, A, X, input_graph):
         self.adjMatrix=A
         self.featMatrix=X.expand(self.sample_size, -1, -1).to('cuda')
         self.N=X.shape[1]
-        self.h=h.expand(self.sample_size, self.N, 32).to('cuda')
         #self.X1=torch.tile(torch.unsqueeze(X, axis=1), [1,K,1]) #(N,K,M)
         
         if len(X.shape)!=3:
@@ -61,14 +59,10 @@ class GIN(nn.Module):
        
         #epsilon generation
         bernoulli=torch.distributions.Bernoulli(torch.tensor([0.5]))
-        if self.noise_dim>0:
-            epsilon=bernoulli.sample(torch.Size([self.sample_size, self.N, self.noise_dim])).view(self.sample_size, self.N, self.noise_dim).to('cuda')
-            temp=torch.cat((self.featMatrix, epsilon, self.h), axis=2).to('cuda')
-        else:
-            epsilon=torch.zeros(self.sample_size, self.N, self.noise_dim).to('cuda')
-            temp=torch.cat((self.featMatrix, epsilon, self.h), axis=2).to('cuda')
-        
-        #for all three GINs : GINu and GINmu and GINsigma
+        epsilon=bernoulli.sample(torch.Size([self.sample_size, self.N, self.noise_dim])).view(self.sample_size, self.N, self.noise_dim).to('cuda')
+        temp=torch.cat((self.featMatrix, epsilon), axis=2).to('cuda') #shape : (sample_size, N, X.shape[2]+64)
+
+        #for GINu
         if self.sample_size>=1:
             outputTensor=torch.zeros([self.sample_size, self.N, self.output_dims[-1]]).to('cuda')
             #self.adjMatrixNumpy=torch.clone(self.adjMatrix)
@@ -78,24 +72,53 @@ class GIN(nn.Module):
                 #print(self.inputGraphGIN.type())
                 self.inputGraphGIN=inputGraphGIN.to('cuda')
                 for layer in self.GINLayers:    
-                 
                   tempdim12=layer.forward(self.inputGraphGIN, tempdim12, edge_weight=None).to('cuda')
-                  tempdim12=torch.mean(tempdim12, dim=0) #Mean READOUT
-                  
+                  #tempdim12=torch.mean(tempdim12, dim=0) #Mean READOUT
                 outputTensor[i]=tempdim12
-        '''
-        if self.sample_size==0:
-            #self.adjMatrixNumpy=torch.clone(self.adjMatrix).numpy()
-            tempdim12=temp.squeeze(0)
-            self.inputGraphGIN=graph_generator(self.adjMatrix, {'feature':self.tempdim12}) 
-            #tempNumpy=temp.squeeze(0).numpy()
-            for layer in self.GINLayers:       
-                tempdim12=layer.forward(self.inputGraphGIN, tempdim12, edge_weight=None)
-                tempdim12=torch.mean(tempdim12, dim=0) #Mean READOUT
-            outputTensor=tempdim12
-        #Return shape : (sample_size, N,D) for GINu / (N,D) for GINmu and GINsigma
-        '''
         return self.activation(outputTensor), epsilon
+
+class GINWithoutNoise(nn.Module):
+    def __init__(self, L, input_dim, output_dims, sample_size, activation=lambda x : x, dropout=0):
+        super(GINWithoutNoise, self).__init__()
+        self.numLayer=L
+        self.input_dim=input_dim
+        self.output_dims=output_dims
+        self.activation=activation
+        self.GINLayers=nn.ModuleList()
+        self.dropoutRate=dropout
+        self.sample_size=sample_size
+        
+        if len(self.output_dims)!=L:
+            raise Exception("number of layer not matched to output dimensions")
+        
+        self.GINLayers.append(dgl.nn.GINConv(apply_func=nn.Linear(self.input_dim, self.output_dims[0]), aggregator_type="sum", init_eps=0, learn_eps=True, activation=self.activation).to('cuda'))
+        for i in range(L-1):
+            self.GINLayers.append(dgl.nn.GINConv(apply_func=nn.Linear(self.output_dims[i], self.output_dims[i+1]),aggregator_type="sum", init_eps=0, learn_eps=True, activation=self.activation).to('cuda'))
+    
+    #return shape : (sample_size, N,D)
+    def forward(self, A, X, input_graph, h):
+        self.adjMatrix=A
+        self.featMatrix=X.expand(self.sample_size, -1, -1).to('cuda')
+        self.N=X.shape[1]
+        self.h=h
+        
+        if len(X.shape)!=3:
+            raise Exception("dimension of input feature matrix is not 2")
+       
+        temp=torch.cat((self.featMatrix, self.h), axis=2).to('cuda') #shape : (sample_size, N, X.shape[2]+32)
+        
+        #for GINmu and GINsigma
+        outputTensor=torch.zeros([self.sample_size, self.N, self.output_dims[-1]]).to('cuda')
+      
+        for i in range(self.sample_size):
+            tempdim12=temp[i,:,:]
+            inputGraphGIN=graph_generator(input_graph, tempdim12) 
+            #print(self.inputGraphGIN.type())
+            self.inputGraphGIN=inputGraphGIN.to('cuda')
+            for layer in self.GINLayers:    
+              tempdim12=layer.forward(self.inputGraphGIN, tempdim12, edge_weight=None).to('cuda')
+            outputTensor[i]=tempdim12
+        return self.activation(outputTensor)
 
 #output : reconstructed adjacency amtrix
 class InnerProductDecoder(nn.Module):
@@ -117,22 +140,50 @@ class BPDecoder(nn.Module):
     def __init__(self, z_dim, J, dropout=0):
         super(BPDecoder, self).__init__()
         self.dropout=nn.Dropout(dropout)
-        self.z_dim=z_dim[-1]
+        self.z_dim=z_dim
+        self.J=J
         #self.distribution=distribution
-        self.rk_logit=torch.nn.Parameter(torch.cuda.FloatTensor(torch.Size([1, self.z_dim])))
+        self.rk_logit=torch.nn.Parameter(torch.cuda.FloatTensor(torch.Size([self.z_dim])))
     #return logit
     def runDecoder(self, Z):
-        rk=torch.sigmoid(self.rk_logit).pow(0.5)
+        torch.nn.init.uniform_(self.rk_logit, a=-6., b=0.)
+        rk=torch.sigmoid(self.rk_logit)
         Z=self.dropout(Z)
-        Z=Z.mul(rk.view(1, 1, self.z_dim))
-        temp=torch.transpose(torch.clone(Z), 1, 2)
-        sigmaInput=torch.bmm(Z, temp)
-        #print(torch.max(sigmaInput))
-        sigmaInput=torch.clamp(sigmaInput, min=float('-inf'), max=10)
-        lambdaterm=torch.exp(sigmaInput)
+        #adj_recon=torch.zeros(self.J, Z.shape[1], Z.shape[1])
+        rk_expand=torch.tile(torch.diag(rk), (self.J,1,1))
+        X=torch.transpose(Z, 1, 2)
+        X=torch.bmm(rk_expand, X)
+        X=torch.bmm(Z, X)
+        X=torch.clamp(X, min=float('-inf'), max=10)
+        adj_recon=1-torch.exp(-torch.exp(X))
+        '''
+        for i, z in enumerate(Z):
+          x = torch.transpose(z, 0, 1)
+          x = torch.matmul(torch.diag(rk), x)
+          x = torch.matmul(z, x)
+          x=torch.clamp(x, min=float('-inf'), max=10)
+          outputs = 1 - torch.exp(- torch.exp(x))
+          adj_recon[i]=outputs
+        '''
+        #ZClone=torch.clone(Z)
+        #diagonal=torch.zeros(self.J, self.z_dim, self.z_dim).to('cuda')
+        #for i, z in enumerate(rk):
+        #  diagonal[i]=torch.diag(z)
+        
+        #temp=torch.bmm(diagonal, torch.transpose(Z, 1, 2)).to('cuda')
+        #sigmaInput=torch.bmm(ZClone, temp)
+        #sigmaInput=torch.clamp(sigmaInput, min=float('-inf'), max=10)
+        #lambdaterm=torch.exp(sigmaInput)
+        #print("^^^^^^^^^^^^")
+        #print(lambdaterm.shape)
         #lambdaterm=torch.clamp(lambdaterm, min=0, max=1000 )
         #A=self.distribution(temp=1, logits=1-torch.exp(-1*lambdaterm))
-        return 1-torch.exp(-1*lambdaterm), Z
+        #adj_recon=1-torch.exp(-1*lambdaterm)
+        
+        #if not self.training:
+        #  adj_recon=torch.mean(adj_recon, dim=0, keepdim=True)
+        
+        return adj_recon, Z, rk
     
 #Lu : number of layers of GINu
 #Lmu : number of layers of GINmu
@@ -146,19 +197,17 @@ class Encoder(nn.Module):
         self.K=K
         self.J=J
         self.noise_dim=noise_dim
-        self.GINu=GIN(Lu, input_dim=input_dim, output_dims=output_dim_u, noise_dim=noise_dim, sample_size=(self.K+self.J),  activation=activation, dropout=0)
-        self.GINmu=GIN(Lmu, input_dim=input_dim, output_dims=output_dim_mu, noise_dim=0, sample_size=(self.K+self.J), activation=lambda x : x, dropout=0)
-        self.GINsigma=GIN(Lsigma, input_dim=input_dim, output_dims=output_dim_sigma, noise_dim=0, sample_size=(self.K+self.J), activation=lambda x : x, dropout=0)
+        self.GINu=GINWithNoise(Lu, input_dim=input_dim, output_dims=output_dim_u, noise_dim=noise_dim, sample_size=(self.K+self.J),  activation=activation, dropout=0)
+        self.GINmu=GINWithoutNoise(Lmu, input_dim=input_dim+output_dim_u[-1], output_dims=output_dim_mu, sample_size=(self.K+self.J), activation=lambda x : x, dropout=0)
+        self.GINsigma=GINWithoutNoise(Lsigma, input_dim=input_dim+output_dim_u[-1], output_dims=output_dim_sigma, sample_size=(self.K+self.J), activation=lambda x : x, dropout=0)
         
     #input X's shape : (1, N, M)
     def encode(self, A, X, input_graph):
-        h=torch.zeros(1, 1, 1).to('cuda')
-        
-        h, epsilon=self.GINu.forward(A, X, input_graph, h)
-        hL=torch.clone(h)
+        h, epsilon=self.GINu.forward(A, X, input_graph)
+        hL=h
         #hL's shape : (K+J, N, output_dim_u)
-        self.mu, zero_eps=self.GINmu.forward(A, X, input_graph, hL)
-        self.sigma, zero_eps=self.GINsigma.forward(A, X, input_graph, hL)
+        self.mu=self.GINmu.forward(A, X, input_graph, hL)
+        self.sigma=self.GINsigma.forward(A, X, input_graph, hL)
         self.sigma=torch.exp(self.sigma * 0.5)
         
         #print("$$$$$$$$$$$$$$$$$$$$$$$$$")
@@ -177,18 +226,18 @@ class Encoder(nn.Module):
         
         param=torch.randn_like(embedding_sigma)
         #Z is equal to emb in the original code
-        Z=param.mul(embedding_sigma).add(embedding_mu).to('cuda')
+        Z=(param*embedding_sigma+embedding_mu).to('cuda')
         epsilon=param
         #print("#################################################")
         #print(Z)
         return Z, self.mu, self.sigma, epsilon
     
 class SIGVAE_GIN(nn.Module):
-    def __init__(self, Lu, Lmu, Lsigma, input_dim, output_dim_u, output_dim_mu, output_dim_sigma, K, J, device, noise_dim=64, decoder_type="bp", activation=nn.functional.relu, dropout=0):
+    def __init__(self, Lu, Lmu, Lsigma, input_dim, output_dim_u, output_dim_mu, output_dim_sigma, K, J, device, noise_dim, decoder_type="bp", activation=nn.functional.relu, dropout=0):
         super(SIGVAE_GIN, self).__init__()
         self.decoder_type=decoder_type
         #self.Rmatrix=Rmatrix
-        self.encoder=Encoder(Lu, Lmu, Lsigma, input_dim, output_dim_u, output_dim_mu, output_dim_sigma, K, J, noise_dim=64, activation=activation, dropout=dropout)
+        self.encoder=Encoder(Lu, Lmu, Lsigma, input_dim, output_dim_u, output_dim_mu, output_dim_sigma, K, J, noise_dim, activation=activation, dropout=dropout)
         self.K=K
         self.J=J
         self.noise_dim=noise_dim #ndim in sigvae-torch
@@ -198,15 +247,15 @@ class SIGVAE_GIN(nn.Module):
         
         if self.decoder_type=="inner":
             self.decoder=InnerProductDecoder(dropout=dropout)
-        if self.decoder_type=="bp":
-            self.decoder=BPDecoder(z_dim=output_dim_mu, J=self.J, dropout=dropout)
+        if self.decoder_type  =="bp":
+            self.decoder=BPDecoder(z_dim=output_dim_mu[-1], J=self.J, dropout=dropout)
 
     def forward(self, adj_matrix, feat_matrix, input_graph):
         self.adj_matrix=adj_matrix
         self.feat_matrix=feat_matrix
         self.latent_representation, self.mu, self.sigma, self.epsilon=self.encoder.encode(adj_matrix, feat_matrix, input_graph)
         if self.decoder_type=="inner":
-            self.generated_prob, self.Z=self.decoder.runDecoder(self.latent_representation)
+            self.generated_prob, self.Z, self.rk=self.decoder.runDecoder(self.latent_representation)
         if self.decoder_type=="bp":
-            self.generated_prob, self.Z=self.decoder.runDecoder(self.latent_representation)
-        return self.generated_prob.to(self.device), self.mu, self.sigma, self.latent_representation, self.Z, self.epsilon
+            self.generated_prob, self.Z, self.rk=self.decoder.runDecoder(self.latent_representation)
+        return self.generated_prob.to(self.device), self.mu, self.sigma, self.latent_representation, self.Z, self.epsilon, self.rk
